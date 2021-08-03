e58d0576516db689011b4a3d2c0199c28e1af87f
Mikael Araya <mikaelaraya12@gmail.com>
2021-08-03 18:34:03 +0300

    Fix megadropdown null children error

--- modules/assortment/components/MegaDropdownColumn.tsx
+++ modules/assortment/components/MegaDropdownColumn.tsx
@@ -49,7 +49,9 @@ const MegaDropdownItem = ({
     <Link href={`/${path.join('/')}`}>
       <a
         className={`mega-link ${
-          type === 'default' && Object.keys(children).length ? 'has-arrow' : ''
+          type === 'default' && Object.keys(children || {}).length
+            ? 'has-arrow'
+            : ''
         }`}
         onMouseEnter={handleMouseEnter}
         onClick={handleClick}
