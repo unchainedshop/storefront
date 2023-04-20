db2afba6c57b92864562951ca67cf974cf8d2807
Mikael Araya <mikaelaraya12@gmail.com>
2023-04-20 16:56:06 +0300

    Change forget password form to react-hook-form

--- modules/auth/components/ForgotPasswordForm.jsx
+++ modules/auth/components/ForgotPasswordForm.jsx
@@ -1,63 +1,100 @@
+import classNames from 'classnames';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { useIntl } from 'react-intl';
 
-import EmailField from '../../forms/components/EmailField';
-import Form from '../../forms/components/Form';
-import FormErrors from '../../forms/components/FormErrors';
-import SubmitButton from '../../forms/components/SubmitButton';
-import useForm from '../../forms/hooks/useForm';
+import Button from '../../common/components/Button';
+import ErrorMessage from '../../common/components/ErrorMessage';
+import useForgotPassword from '../hooks/useForgotPassword';
 
-const ForgotPasswordForm = ({ onSubmit, onSubmitSuccess = () => true }) => {
+const ForgotPasswordForm = () => {
   const { formatMessage } = useIntl();
 
-  const form = useForm({
-    submit: onSubmit,
-    onSubmitSuccess,
-    getSubmitErrorMessage: async () => {
-      form.formik.setFieldError(
-        'email',
-        formatMessage({
-          id: 'email_address_not_exist',
-          defaultMessage: 'Provided email does not exist',
-        }),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+  const { forgotPassword } = useForgotPassword();
+
+  const onSubmit = async ({ email }) => {
+    try {
+      await forgotPassword({ email });
+      toast.success(
+        formatMessage(
+          {
+            id: 'reset_link_sent',
+            defaultMessage: 'Password reset link sent to {email} ',
+          },
+          {
+            email,
+          },
+        ),
       );
-    },
-    initialValues: {
-      email: '',
-    },
-  });
+    } catch (e) {
+      if (e.message?.toLowerCase()?.includes('user not found')) {
+        setError('email', {
+          type: 'manual',
+          message: formatMessage({
+            id: 'email_address_not_exist',
+            defaultMessage: 'Provided email does not exist',
+          }),
+        });
+      }
+    }
+  };
+
   return (
-    <Form
-      form={form}
-      className="mt-8 space-y-6 px-4 pt-3 pb-8 shadow sm:rounded-lg sm:px-10"
-    >
-      <div>
-        <p className="py-4">
-          {formatMessage({
-            id: 'forgot_password_header_description',
-            defaultMessage:
-              "Enter your email address below and we'll send you a link to reset your password!",
-          })}
-        </p>
-        <EmailField
-          name="email"
-          id="email-address"
-          label={formatMessage({
-            id: 'email_address',
-            defaultMessage: 'Email Address',
-          })}
-          placeholder={formatMessage({
-            id: 'email_address',
-            defaultMessage: 'Email Address',
-          })}
-          required
-        />
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+      <p className="py-4">
+        {formatMessage({
+          id: 'forgot_password_header_description',
+          defaultMessage:
+            "Enter your email address below and we'll send you a link to reset your password!",
+        })}
+      </p>
+      <div className="form-row">
+        <div className={`mb-3 col-md-6 ${errors.email ? 'text-red-500' : ''}`}>
+          <label className="form-label">
+            {formatMessage({
+              id: 'email',
+              defaultMessage: 'Email',
+            })}
+          </label>
+          <input
+            className={classNames(
+              'relative mt-1 block w-full dark:focus:autofill dark:hover:autofill dark:autofill  dark:placeholder:text-white dark:bg-slate-900 dark:text-slate-200 appearance-none rounded-md border-2 border-slate-200 dark:border-slate-700 px-4 py-2.5 text-sm text-slate-900 placeholder-slate-400 shadow-sm placeholder:text-slate-400 focus:border-slate-400 focus:outline-none focus:ring-1 focus:ring-slate-400',
+
+              {
+                'border-2 border-color-danger-600  placeholder:text-red-500':
+                  !!errors?.email,
+              },
+            )}
+            name="email"
+            id="email-address"
+            type="email"
+            defaultValue=""
+            label={formatMessage({
+              id: 'email_address',
+              defaultMessage: 'Email Address',
+            })}
+            placeholder={formatMessage({
+              id: 'email_address',
+              defaultMessage: 'Email Address',
+            })}
+            {...register('email', { required: true })}
+          />
+        </div>
       </div>
-      <FormErrors />
-      <div className="mb-6">
-        <SubmitButton
-          className="w-full "
-          label={formatMessage({
+      {errors?.email && <ErrorMessage message={errors.email?.message} />}
+      <div className="mb-6 mt-6">
+        <Button
+          disabled={Object.keys(errors).length}
+          type="submit"
+          className="flex w-full justify-center rounded-md border border-transparent bg-slate-800 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2"
+          text={formatMessage({
             id: 'send_rest_link',
             defaultMessage: 'Send reset link',
           })}
@@ -77,7 +114,7 @@ const ForgotPasswordForm = ({ onSubmit, onSubmitSuccess = () => true }) => {
         </Link>
         .
       </p>
-    </Form>
+    </form>
   );
 };
 
